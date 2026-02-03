1aca8cc5aa213483de8f4331bf92d6a5d56d5dc6
Anchal Singh <anchalsingh24032001@gmail.com>
2026-02-03 09:49:30 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1mg0YVA73WmvBHaVWjCKiP0pxlbyGW1SM/view"
+            href="https://drive.google.com/file/d/136ipZs9IN5nKBpiui-6QDKoVyi4YpVxb/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
